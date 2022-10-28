eb580cb15ca70de9958ce776d8c46c4671b8388a
Gus <gustavosayago@insi.edu.ar>
2022-10-27 21:17:01 -0300

    Version1!

---
+++ ruperto/ruperto.py
@@ -0,0 +1,50 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import pyaudio
+
+name = "ruperto"
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+def listen():
+    listener = sr.Recognizer()     
+    with sr.Microphone() as source:
+        print("Escuchando...")
+        listener.adjust_for_ambient_noise(source)
+        pc = listener.listen(source)
+
+    try:
+        rec = listener.recognize_google(pc, language="es")
+        rec = rec.lower()
+    except sr.UnknownValueError:
+        print("No te entendí, intenta de nuevo")
+        if name in rec:
+            rec = rec.replace(name, '')
+    return rec
+
+def run_Ruperto():
+     while True:
+        try:
+            rec = listen()
+        except UnboundLocalError:
+            print("No te entendí, intenta de nuevo")
+            continue     
+        if 'reproduce' in rec:
+            music = rec.replace('reproduce', '')
+            print("Reproduciendo " + music)
+            talk("Reproduciendo " + music)
+            pywhatkit.playonyt(music)
+
+if __name__ == '__main__':
+    run_Ruperto()
+    
+                
+            
